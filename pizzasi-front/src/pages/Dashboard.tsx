1c8ede4c2cb0334b51286dd86f1509e500ae0429
EmiLy-Ly-san <emilie.deduyver@gmail.com>
2025-12-14 22:27:54 +0100

    card ordertracking + dashboard integration

--- pizzasi-front/src/pages/Dashboard.tsx
+++ pizzasi-front/src/pages/Dashboard.tsx
@@ -1,38 +1,11 @@
-export default function PizzaOfTheMomentCard() {
+import PizzaMomentCard from "../components/PizzaMomentCard";
+import OrderTrackingCard from "../components/OrderTrackingCard";
+
+export default function Dashboard() {
   return (
-    <section className="px-5 mt-6">
-      {/* Card */}
-      <div className=" relative overflow-hidden rounded-3xl bg-[#ad252e] px-5 py-6">
-        {/* Text */}
-        <div className=" max-w-[65%]">
-          <h2 className="text-l text-white">
-            Avez-vous vu
-            <br />
-            notre offre du moment ?
-          </h2>
-
-          <p className="mt-2 text-xs text-white/80">
-            Proposez votre recette et
-            <br />
-            devenez la Pizza du Trimestre !
-          </p>
-
-          <button className="mt-4 inline-flex items-center rounded-full bg-white w-[153] h-[38] py-2 px-4 text-xs text-[#ad252e]">
-            En savoir plus
-          </button>
-        </div>
-        {/* Dots */}
-        <div className="mt-6 flex justify-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-white/90" />
-          <span className="h-2 w-2 rounded-full bg-white/40" />
-          <span className="h-2 w-2 rounded-full bg-white/40" />
-        </div>
-      </div>
-      <img
-        src="/images/pizza1.svg"
-        alt="Pizza du moment"
-        className="absolute -right-4 top-[280px] h-32 w-48 -translate-y-1/2 rotate-6 "
-      />
-    </section >
+    <div className="pb-32">
+      <PizzaMomentCard />
+      <OrderTrackingCard />
+    </div>
   );
 }
