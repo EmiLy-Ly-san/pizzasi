f1fbd103ea47b1332037a0266a6e9e6d4f4e40b7
EmiLy-Ly-san <emilie.deduyver@gmail.com>
2025-12-14 20:36:19 +0100

    footer

--- pizzasi-front/src/components/footer.tsx
+++ pizzasi-front/src/components/footer.tsx
@@ -1,5 +1,39 @@
-export default function Footer() {
+import { NavLink } from "react-router-dom";
+
+export default function BottomNav() {
   return (
-    <h2>footer</h2>
-  )
+    <nav className="fixed bottom-6 left-1/2 z-50 w-[92%] max-w-md -translate-x-1/2">
+      <div className="relative rounded-full bg-zinc-900 p-1 shadow-lg">
+        <div className="flex items-center justify-between">
+          {/* HOME */}
+          <NavLink to="/dashboard" end>
+            {({ isActive }) => (
+              <img
+                src={
+                  isActive
+                    ? "/icons/home-active.svg"
+                    : "/icons/home.svg"
+                }
+                alt="Home"
+              />
+            )}
+          </NavLink>
+
+          <button aria-label="Panier">
+            <img src="/icons/cart-icon.svg" alt="cart" />
+          </button>
+
+          <button aria-label="Profil">
+            <img src="/icons/profil-icon.svg" alt="cart" />
+          </button>
+
+          <button
+            aria-label="Menu"
+          >
+            <img src="/icons/menu-icon.svg" alt="menu" />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
