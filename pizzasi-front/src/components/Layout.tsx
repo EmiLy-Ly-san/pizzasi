a4faa5ff023f9fa0e12d1d0a996e7e8723cb15b6
EmiLy-Ly-san <emilie.deduyver@gmail.com>
2025-12-15 18:58:13 +0100

    subscription card + corrections casing

--- pizzasi-front/src/components/Layout.tsx
+++ pizzasi-front/src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom';
-import Header from './Header.tsx';
-import Footer from './Footer.tsx';
+import Header from './HeaderGood';
+import Footer from './FooterGood';
+
+
 
 export default function Layout() {
   return (
--- pizzasi-front/src/pages/Dashboard.tsx
+++ pizzasi-front/src/pages/Dashboard.tsx
@@ -1,11 +1,13 @@
 import PizzaMomentCard from "../components/PizzaMomentCard";
 import OrderTrackingCard from "../components/OrderTrackingCard";
+import SubscriptionCard from "../components/SubscriptionPage";
 
 export default function Dashboard() {
   return (
     <div className="pb-32">
       <PizzaMomentCard />
       <OrderTrackingCard />
+      <SubscriptionCard />
     </div>
   );
 }
