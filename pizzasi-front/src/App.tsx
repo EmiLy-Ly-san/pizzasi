1403820829166eca5c6659c235d18a66488e290f
EmiLy-Ly-san <emilie.deduyver@gmail.com>
2025-12-16 14:44:17 +0100

    navlink footer

--- pizzasi-front/src/App.tsx
+++ pizzasi-front/src/App.tsx
@@ -2,12 +2,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import Cart from './pages/Cart';
+import Home from './pages/Home';
 
 const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
-      { path: '/', element: <Dashboard /> },
+      { path: '/', element: <Home /> },
+      { path: '/dashboard', element: <Dashboard /> },
       { path: '/cart', element: <Cart /> },
     ],
   },
