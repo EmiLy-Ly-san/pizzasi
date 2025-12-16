b4d49fc21b2abe44f60b234ddd406bc7c9cf7a6c
Julie Debonnefoy <julie.debonnefoy.pro@gmail.com>
2025-12-16 22:07:00 +0100

    home sweet home

--- pizzasi-front/src/pages/home.tsx
+++ pizzasi-front/src/pages/home.tsx
@@ -1,10 +1,7 @@
-import PizzaCatalog from "../components/PizzaCatalog";
-
 export default function Home() {
   return (
     <div className="h-main">
       <h1>Home</h1>
-      <PizzaCatalog />
     </div>
   )
 }
